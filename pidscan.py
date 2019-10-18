820241ba8a5065be09886c8c5bf08e2b5a53f256
SteveDonegan <steve.donegan@stfc.ac.uk>
2019-10-18 16:50:19 +0100

    First dev iteration to try and traverse ppid tree

--- pidscan.py
+++ pidscan.py
@@ -4,32 +4,55 @@ import sys
 #from https://stackoverflow.com/questions/3332043/obtaining-pid-of-child-process
 
 def childproc(parent_pid, sig=signal.SIGTERM):
-	
-	children = []
 
-        ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % parent_pid, shell=True, stdout=subprocess.PIPE)
-        ps_output = ps_command.stdout.read()
-        retcode = ps_command.wait()
-        assert retcode == 0, "ps command returned %d" % retcode
+    children = []
 
-        for pid_str in ps_output.split("\n")[:-1]:
-                children.append(pid_str)
+    ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % parent_pid, shell=True, stdout=subprocess.PIPE)
+    ps_output = ps_command.stdout.read()
+    retcode = ps_command.wait()
+    assert retcode == 0, "ps command returned %d" % retcode
+
+    for pid_str in ps_output.split("\n")[:-1]:
+        children.append(pid_str)
+
+    return children
+
+
 
 ppid = int(sys.argv[1])
-d={}
+process_map = {}
+no_children = False
+child = None
 
-try:
-	d[ppid] = childproc(ppid)
+while not no_children:
 
-except:
-	print 'here'
-	d[ppid] = None
-import pdb; pdb.set_trace()
-for i in d.keys():
-	if d[i] is not None:
-		for j in d[i]:
-			print "%s has child process: %s" %(i, j)
-	
-	else:
-		print 'oops'
+    #initialise the top level
+    '''
+    if cnt == 0:
+        parent = ppid
+        
+    elif child != None:
+        parent = child # this is going to get confusing..... 
+        
+    '''
+
+    if child is None:
+        parent = ppid
+
+    #first pass at first level of child processes
+    try:
+        child = childproc(parent)
+        process_map[parent] = child
+
+        #reset parent to the child ppid to get the next step down,
+
+    except:
+        no_children = True
+
+
+for i in process_map.keys():
+    if process_map[i]:
+        for j in process_map[i]:
+            print "%s has child process: %s" %(i, j)
+
 
