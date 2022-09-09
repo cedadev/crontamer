a5f6f67ba1baa3591c6afcecbb449157b0d7992a
Sam Pepler <sam.pepler@stfc.ac.uk>
2022-09-09 13:20:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='crontamer',
-    version='1.0.1',
+    version='1.0.2',
     description='A wrapper script for cron processes. Stops multiple process instances and gives a default timesout.',
     long_description=long_description,
 
